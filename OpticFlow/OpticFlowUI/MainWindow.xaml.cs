21a84535cf20901bd2b07561b30ed61a928f94e7
Prelly95 <Prelly95@gmail.com>
2019-02-16 23:21:52 +1100

    First commit

---
+++ OpticFlow/OpticFlowUI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.XFeatures2D;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.Util.TypeEnum;
+using Emgu.CV.Tracking;
+
+namespace OpticFlowUI {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
